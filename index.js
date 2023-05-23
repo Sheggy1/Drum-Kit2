05137ded32aa6924023518ab82b1346425e50443
Sheggybtw <pschenichniyvitaliy@gmail.com>
2023-05-23 15:53:04 +0300

    Main logic

---
+++ index.js
@@ -0,0 +1,8 @@
+let numbersOfBtn = document.querySelectorAll(".drum").length;
+
+for(let i = 0; i < numbersOfBtn; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        
+    });    
+}
+
